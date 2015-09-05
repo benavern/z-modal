584dcd4c183d411a63162d04f96bc688617f28bb
benavern <bencrdc@gmail.com>
2015-09-05 15:34:11 +0200

    improved header layout + documentations ...

--- js/app.js
+++ js/app.js
@@ -1,32 +1,38 @@
 (function() {
   createSocialLinks();
+  demo1();
 })();
 
+// social links made easy
 function createSocialLinks() {
   var here = window.location.href;
   var URL = {
-    f: "http://www.facebook.com/sharer.php?u=" + here,
-    t: "https://twitter.com/share?url="+ here +"&text=Just%20found%20something%20awesome!&via=benavern",
-    g: "https://plus.google.com/share?url=" + here,
-    m: "mailto:?subject=Just%20found%20something%20awesome!&body=Hey!%20What%20do%20you%20think%20about%20" + here + " ?"
+    facebook: "http://www.facebook.com/sharer.php?u=" + here,
+    twitter: "https://twitter.com/share?url="+ here +"&text=Just%20found%20something%20awesome!&via=benavern",
+    googlePlus: "https://plus.google.com/share?url=" + here,
+    mail: "mailto:?subject=Just%20found%20something%20awesome!&body=Hey!%20What%20do%20you%20think%20about%20" + here + " ?"
   }
-  var socialPlace = document.getElementById('social-wrapper');
-  for(var x in URL){
-    var el = document.createElement("a");
-    el.className = "social-btn " + x;
-    el.href = URL[x];
-    // el.innerHTML = x;
-    socialPlace.appendChild(el);
+  var socialPlaces = document.getElementsByClassName('social-wrapper');
+  for(var n=0; n<socialPlaces.length; n++){
+    var sp = socialPlaces[n];
+    for(var x in URL){
+      var el = document.createElement("a");
+      el.title = "Share via " + x;
+      el.className = "social-btn " + x;
+      el.href = URL[x];
+      sp.appendChild(el);
+    }
   }
+
 }
 
+// demos for z-modal plugin
 
-
-
-var demo;
-var modalArgs = {};
-var btnTest = document.getElementById('demo1');
-
-btnTest.addEventListener('click', function(e){
-  demo = new ZMODAL(modalArgs);
-})
+function demo1(){
+  var demo;
+  var modalArgs = {};
+  var btnTest = document.getElementById('demo1');
+  btnTest.addEventListener('click', function(e){
+    demo = new ZMODAL(modalArgs);
+  })
+}
