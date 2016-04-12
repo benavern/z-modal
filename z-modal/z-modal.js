22acdf54db8c87d0195e0d2a12cc3cb7739753cb
I-NOZex <simplesmente_moi@hotmail.com>
2016-04-12 14:24:01 +0100

    Fix: Validation if className isn't empty, before call classList.add()

--- z-modal/z-modal.js
+++ z-modal/z-modal.js
@@ -107,7 +107,9 @@
       (function(i) {
         var theBtn = _this.options.buttons[i];
         var btn = document.createElement("div");
-        btn.classList.add("z-modal-btn",theBtn.className);
+        btn.className = "z-modal-btn";
+		if(theBtn.className && theBtn.className.length > 0)
+			theBtn.classList.add(theBtn.className);
         btn.innerHTML = theBtn.label;
         if(theBtn.half === true){
           btn.classList.add("z-modal-btn-half");
